fefeede96bb1cf7668d5e649b71009700a961013
Roman Zajavkowy <roman@zajavka.pl>
2025-04-19 19:12:56 +0100

    commit -6

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello2!");
+        System.out.println("Hello3!");
+        System.out.println("Hello5!");
+        System.out.println("Hello5!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
+        System.out.println("Hello7!");
+        System.out.println("Hello8!");
+        System.out.println("Hello8!");
+        System.out.println("Hello8!");
+        System.out.println("Hello8!");
+    }
+}
