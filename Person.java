6ae2a38a164543160a6e0338bfc0ea569363c859
Stefan Zajavkowy <stefan@zajavka.pl>
2025-04-19 20:22:35 +0100

    commit -3

--- Person.java
+++ Person.java
@@ -5,6 +5,16 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
+        System.out.println("Hello2!");
         System.out.println("Hello3!");
         System.out.println("Hello5!");
         System.out.println("Hello5!");
