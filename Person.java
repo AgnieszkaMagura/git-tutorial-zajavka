1ee01235c36d743efbbde1f2a10f4b5ec41bfa93
Roman Zajavkowy <roman@zajavka.pl>
2025-05-17 17:02:35 +0100

    commit: hello roman

--- Person.java
+++ Person.java
@@ -3,6 +3,7 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello!");
+        System.out.println("Hello I'm Roman!");
 
     }
 }
